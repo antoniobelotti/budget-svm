78a3da06f7f7097cc115b6d2611e3d9d57decd37
antoniobelotti <antonio.belotti@studenti.unimi.it>
2023-10-23 10:50:19 +0200

    delete experiments scripts and config files

--- experiments/utils.py
+++ experiments/utils.py
@@ -15,14 +15,13 @@ from budgetsvm.kernel import (
     LinearKernel,
     PrecomputedKernel,
 )
+import budgetsvm.kernel
 
 logger = logging.getLogger("experiments")
 
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
-        if "kernel" in type(obj).__name__.lower():
-            return str(obj)
         if isinstance(obj, UUID):
             return str(obj)
         if isinstance(obj, np.integer):
@@ -33,88 +32,11 @@ class CustomJSONEncoder(json.JSONEncoder):
             return obj.tolist()
         if dataclasses.is_dataclass(obj):
             return dataclasses.asdict(obj)
+
+        # FIXME: why isinstance(obj, Kernel) does not work?
+        if "kernel" in type(obj).__name__.lower():
+            return str(obj)
         return super().default(obj)
-
-
-def run_cvgrid(
-    model, precomputed_X_train, precomputed_X_test, y_train, y_test, kernel, cfg
-):
-    skf = StratifiedKFold(
-        n_splits=cfg.get("cv", 5), shuffle=True, random_state=cfg.get("seed", 42)
-    )
-    c_values = cfg.get("C", [1.0])
-
-    cvgrid = GridSearchCV(
-        model,
-        {"C": c_values, "kernel": [kernel]},
-        refit=True,
-        verbose=0,
-        cv=skf,
-        n_jobs=10,
-    )
-    try:
-        cvgrid.fit(precomputed_X_train, y_train)
-        test_score = cvgrid.score(precomputed_X_test, y_test)
-        return cvgrid.best_estimator_, cvgrid.best_params_, test_score
-    except FitFailedWarning:
-        pass
-    except Exception as e:
-        logger.error("GridSearchCV failed with unexpected error.")
-        logger.error(traceback.format_exc())
-    return None, None, 0
-
-
-def model_selection(model, X_train, X_test, y_train, y_test, cfg):
-    kernelcfg = cfg.get("kernels", {"linear"})
-    num_params = len(cfg.get("C", [1.0])) * (
-        len(kernelcfg.get("gaussian", []))
-        + len(kernelcfg.get("polynomial", []))
-        + (1 if "linear" in kernelcfg else 0)
-    )
-
-    logger.debug(
-        f"Launching GridSearcCV on {model} - {num_params} params, {cfg.get('cv', 5)}-folds, "
-        f"for a total of {num_params * cfg.get('cv', 5)} fit calls."
-    )
-
-    best_estimator = None
-    best_score = 0.0
-    best_params = None
-
-    all_kernels = []
-    for kernel_name, hp in cfg.get("kernels", {"linear"}).items():
-        match kernel_name:
-            case "linear":
-                all_kernels.append(PrecomputedKernel(original_kernel=LinearKernel()))
-            case "gaussian":
-                for v in hp:
-                    all_kernels.append(PrecomputedKernel(original_kernel=GaussianKernel(v)))
-            case "polynomial":
-                for v in hp:
-                    all_kernels.append(PrecomputedKernel(original_kernel=PolynomialKernel(v)))
-
-    for krn in all_kernels:
-        precomputed_X_train = np.array(
-            [[krn.compute(x, y) for y in X_train] for x in X_train]
-        )
-        precomputed_X_test = np.array(
-            [[krn.compute(x, y) for y in X_train] for x in X_test]
-        )
-        est, par, score = run_cvgrid(
-            model,
-            precomputed_X_train,
-            precomputed_X_test,
-            y_train,
-            y_test,
-            krn,
-            cfg,
-        )
-        if score > best_score:
-            best_estimator = est
-            best_params = par
-            best_score = score
-
-    return best_estimator, best_params, best_score
 
 
 class Timer:
